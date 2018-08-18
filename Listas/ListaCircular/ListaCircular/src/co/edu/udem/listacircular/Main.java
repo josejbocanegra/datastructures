1696aadf48b4ba9baddc7f7c33ececf6bccc344e
josebocanegra <josebocanegra@192.168.1.5>
2018-08-18 15:17:46 -0500

    Without throws

--- Listas/ListaCircular/ListaCircular/src/co/edu/udem/listacircular/Main.java
+++ Listas/ListaCircular/ListaCircular/src/co/edu/udem/listacircular/Main.java
@@ -3,7 +3,7 @@ package co.edu.udem.listacircular;
 import co.edu.udem.listacircular.exceptions.NodoNoExisteException;
 
 public class Main {
-	public static void main(String[] args ) throws NodoNoExisteException {
+	public static void main(String[] args ){
 		ListaCircular listaCircular = new ListaCircular();
 		listaCircular.insertar(new Nodo(50));
 		listaCircular.insertar(new Nodo(150));
