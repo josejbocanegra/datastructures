c23869f43b7159cdb5f0540d46f8d7b6389ee3a7
josebocanegra <josebocanegra@192.168.1.9>
2018-09-02 11:09:31 -0500

    MCD

--- Recursividad/src/co/edu/udem/recursividad/Main.java
+++ Recursividad/src/co/edu/udem/recursividad/Main.java
@@ -3,9 +3,13 @@ package co.edu.udem.recursividad;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Recursividad r = new Recursividad();
-		System.out.println(r.factorial(5));
+		/*System.out.println(r.factorial(5));
+		System.out.println(r.sumatoria(100));
+		r.printNumber(1,100);*/
+		System.out.println(r.fibonacci(8));
+		System.out.println(r.mcd(100, 200));
 	}
 
 }
--- Recursividad/src/co/edu/udem/recursividad/Recursividad.java
+++ Recursividad/src/co/edu/udem/recursividad/Recursividad.java
@@ -6,4 +6,27 @@ public class Recursividad {
 		
 		return f == 0 ? 1 : f * factorial (f -1);
 	}
+	
+	public int sumatoria (int s) {
+		
+		return s == 0 ?  0 : s + sumatoria (s - 1);
+	}
+	
+	public void printNumber (int a, int b) {
+		System.out.println(a);
+		if(a < b)
+			printNumber(a + 1, b);
+	}
+	
+	public int fibonacci (int f) {
+		
+		return f < 2 ? f : fibonacci(f - 1) + fibonacci(f - 2);
+	}
+	
+	public int mcd (int a, int b) {
+		
+		return a == b ? a : mcd (a < b ? a : b, a > b ? (a - b) : (b - a) );
+		
+	}
 }
+
