623957ad5c3d157ec9a5cbaab864516c78a99277
josebocanegra <josebocanegra@192.168.1.9>
2018-09-02 10:12:45 -0500

    Recursividad

---
+++ Recursividad/src/co/edu/udem/recursividad/Main.java
@@ -0,0 +1,11 @@
+package co.edu.udem.recursividad;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Recursividad r = new Recursividad();
+		System.out.println(r.factorial(5));
+	}
+
+}
